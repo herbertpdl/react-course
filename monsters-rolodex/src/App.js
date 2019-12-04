140e94345ca9083dabb1141dec77e1ba29b7eeb7
Herbert Lago <hebertpdl@gmail.com>
2019-12-04 08:36:38 -0300

    Adding input field
    Signed-off-by: Herbert Lago <hebertpdl@gmail.com>

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
   }
 
@@ -23,6 +24,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField:  e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     )
