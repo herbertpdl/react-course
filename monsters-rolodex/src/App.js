b4b2872eca4dfe328d559f4b6aa3f074285fa4c8
Herbert Lago <herbert.lago@leads2b.com>
2019-12-04 17:53:01 -0300

    Add searchbox component
    Signed-off-by: Herbert Lago <herbert.lago@leads2b.com>

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -22,14 +23,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monsters"
-          onChange={e => this.setState({ searchField:  e.target.value })}
+          handleChange={e => this.setState({ searchField: e.target.value})}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
