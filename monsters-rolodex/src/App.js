e563bde2d46b9d814b4588769a9d5b2d6eabf893
Herbert Lago <herbert.lago@leads2b.com>
2019-12-05 17:12:46 -0300

    Add handlechange method
    Signed-off-by: Herbert Lago <herbert.lago@leads2b.com>

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -19,7 +19,10 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
+  }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
   }
 
   render() {
@@ -30,9 +33,10 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
