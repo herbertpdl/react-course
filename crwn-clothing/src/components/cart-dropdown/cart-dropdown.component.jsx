1850d89c2d4eefcf0e62d70275e5d40405bc4998
Herbert Lago <hebertpdl@gmail.com>
2020-05-17 18:51:13 -0300

    Aplying new cart functionalities and redux-persist
    Signed-off-by: Herbert Lago <hebertpdl@gmail.com>

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
