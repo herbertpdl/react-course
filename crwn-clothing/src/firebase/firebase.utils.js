8796faf42374f2a97fbfcdc023b3b4d6455a2724
herbertpdl <hebertpdl@gmail.com>
2021-01-18 08:14:55 -0300

    add: method to pull data from products collection and convert it to a new custom array

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -50,6 +50,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   })
 
   return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  console.log(transformedCollection);
 }
 
 export const auth = firebase.auth();
